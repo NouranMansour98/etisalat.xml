fae016cf08383a39d5f42ab4359aea72685ea88d
NouranMansour98 <nouranmansour98@gmail.com>
2024-08-21 23:56:49 +0300

    Update WebAppAutomationTest.java

--- src/test/java/etisalat/automation/WebAppAutomationTest.java
+++ src/test/java/etisalat/automation/WebAppAutomationTest.java
@@ -13,13 +13,9 @@ import org.testng.annotations.Test;
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import com.aventstack.extentreports.ExtentTest;
-
 import org.openqa.selenium.chrome.ChromeOptions;
-
 import java.time.Duration;
 import java.util.List;
-
-// NEW
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 
@@ -32,25 +28,18 @@ public class WebAppAutomationTest {
     @BeforeClass
     public void setUp() {
 
-
-// In your setUp method
         WebDriverManager.chromedriver().setup();
 
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless"); // Run in headless mode
+        options.addArguments("--headless"); 
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--disable-gpu");
         options.addArguments("--window-size=1920x1080");
 
         driver = new ChromeDriver(options);
-//        driver = new ChromeDriver();
-
-//        System.setProperty("webDriver.chrome.driver", "path/to/chromedriver");
-//        driver = new ChromeDriver();
         wait = new WebDriverWait(driver, Duration.ofSeconds(50));
 
-        // Initialize reporting
         ExtentSparkReporter spark = new ExtentSparkReporter("Spark.html");
         extentReports = new ExtentReports();
         extentReports.attachReporter(spark);
@@ -60,50 +49,32 @@ public class WebAppAutomationTest {
     public void testLoginAndNavigate() {
         extentTest = extentReports.createTest("Login and Navigation Test");
 
-        // Open login page
         driver.get("https://the-internet.herokuapp.com/login");
-        // driver.get("what");
         extentTest.info("Opened login page.");
 
-        // Login
         WebElement usernameField = driver.findElement(By.id("username"));
         WebElement passwordField = driver.findElement(By.id("password"));
         WebElement loginButton = driver.findElement(By.cssSelector("button[type='submit']"));
 
         usernameField.sendKeys("tomsmith");
         passwordField.sendKeys("SuperSecretPassword!");
-//        usernameField.sendKeys("NouranHM");
-//        passwordField.sendKeys("Letsgo123!");
         loginButton.click();
 
-        // Wait for secure area
         wait.until(ExpectedConditions.urlContains("/secure"));
         extentTest.info("Logged in successfully.");
 
-        // Check for secure area content
         WebElement secureAreaMessage = driver.findElement(By.cssSelector(".flash.success"));
         Assert.assertTrue(secureAreaMessage.isDisplayed(), "Secure area message is not displayed.");
 
-        // Navigate to tables page
         driver.get("https://the-internet.herokuapp.com/tables");
         extentTest.info("Opened tables page.");
 
-        // Extract data from table
         WebElement table = driver.findElement(By.id("table1"));
         List<WebElement> rows = table.findElements(By.tagName("tr"));
         for (WebElement row : rows) {
             System.out.println(row.getText());
         }
 
-        // Navigate to inputs page and submit form
-        // driver.get("https://the-internet.herokuapp.com/inputs");
-        // extentTest.info("Opened inputs page.");
-        // WebElement inputField = driver.findElement(By.cssSelector("input[type='number']"));
-        // WebElement submitButton = driver.findElement(By.cssSelector("button"));
-
-        // inputField.sendKeys("12345");
-        // submitButton.click();
-        // extentTest.info("Filled out and submitted form.");
 
         driver.get("https://the-internet.herokuapp.com/inputs");
         extentTest.info("Opened inputs page.");
